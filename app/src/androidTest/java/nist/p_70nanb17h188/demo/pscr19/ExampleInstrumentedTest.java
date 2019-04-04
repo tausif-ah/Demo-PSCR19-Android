61d4d21b5bca849ef268f269e8a7f070ffac3265
Tausif <ctausif@gmail.com>
2019-04-04 11:38:03 -0400

    Initial commit

---
+++ app/src/androidTest/java/nist/p_70nanb17h188/demo/pscr19/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package nist.p_70nanb17h188.demo.pscr19;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("nist.p_70nanb17h188.demo.pscr19", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/nist/p_70nanb17h188/demo/pscr19/MainActivity.java
@@ -0,0 +1,13 @@
+package nist.p_70nanb17h188.demo.pscr19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
