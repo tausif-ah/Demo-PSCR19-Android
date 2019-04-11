0c1c080804a219c2d92c12bf871ca9fe5eb08c04
Tausif <ctausif@gmail.com>
2019-04-11 11:58:40 -0400

    Link layer classes added

---
+++ app/src/main/java/nist/p_70nanb17h188/demo/pscr19/link/DataReceivedHandler.java
@@ -0,0 +1,12 @@
+package nist.p_70nanb17h188.demo.pscr19.link;
+
+public interface DataReceivedHandler {
+
+    /**
+     * Called when a piece of data is received from a neighbor.
+     *
+     * @param id id of the sender neighbor.
+     * @param data data received.
+     */
+    void dataReceived(NeighborID id, byte[] data);
+}
