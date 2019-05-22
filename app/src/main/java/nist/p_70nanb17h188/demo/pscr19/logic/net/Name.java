94977917a459ae9e80e1d65b4c40f06bbad048e2
Jiachen Chen <jiachen@winlab.rutgers.edu>
2019-05-22 13:50:31 -0400

    Minor updates

--- app/src/main/java/nist/p_70nanb17h188/demo/pscr19/logic/net/Name.java
+++ app/src/main/java/nist/p_70nanb17h188/demo/pscr19/logic/net/Name.java
@@ -1,31 +1,13 @@
 package nist.p_70nanb17h188.demo.pscr19.logic.net;
 
-import android.os.Parcel;
-import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
-public class Name implements Parcelable {
-    public final long value;
+public class Name {
+    private final long value;
 
     public Name(long value) {
         this.value = value;
     }
-
-    private Name(Parcel in) {
-        value = in.readLong();
-    }
-
-    public static final Creator<Name> CREATOR = new Creator<Name>() {
-        @Override
-        public Name createFromParcel(Parcel in) {
-            return new Name(in);
-        }
-
-        @Override
-        public Name[] newArray(int size) {
-            return new Name[size];
-        }
-    };
 
     public boolean isMulticast() {
         return value < 0;
@@ -59,13 +41,4 @@ public class Name implements Parcelable {
         return "Name{" + "value=" + value + '}';
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeLong(value);
-    }
 }
