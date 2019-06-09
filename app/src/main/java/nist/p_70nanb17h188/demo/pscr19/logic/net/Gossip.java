60413f8efe1cf4d7132c876245e558844de8c808
Mohammad <->
2019-06-08 20:05:31 -0700

    net layer

---
+++ app/src/main/java/nist/p_70nanb17h188/demo/pscr19/logic/net/Gossip.java
@@ -0,0 +1,18 @@
+package nist.p_70nanb17h188.demo.pscr19.logic.net;
+
+import android.support.annotation.NonNull;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import nist.p_70nanb17h188.demo.pscr19.Helper;
+import nist.p_70nanb17h188.demo.pscr19.imc.Context;
+import nist.p_70nanb17h188.demo.pscr19.imc.Intent;
+import nist.p_70nanb17h188.demo.pscr19.imc.IntentFilter;
+import nist.p_70nanb17h188.demo.pscr19.logic.link.LinkLayer;
+import nist.p_70nanb17h188.demo.pscr19.logic.link.NeighborID;
+
+public class Gossip {
+
+}
--- app/src/main/java/nist/p_70nanb17h188/demo/pscr19/logic/net/Name.java
+++ app/src/main/java/nist/p_70nanb17h188/demo/pscr19/logic/net/Name.java
@@ -17,6 +17,10 @@ public class Name {
         this.value = value;
     }
 
+    public Name (String value){
+        this.value = Long.parseLong(value);
+    }
+
     public boolean isMulticast() {
         return value < 0;
     }
